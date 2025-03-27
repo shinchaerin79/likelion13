21a1eba0e66bb1e1dc7a9471dfc3f60c252020fd
shinchaerin79 <166346480+shinchaerin79@users.noreply.github.com>
2025-03-27 15:30:35 +0900

    :recycle: Refactor: conflict test

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World! Did a conflict occur?");
     }
 }
