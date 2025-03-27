05cb802e1a3589af5081b71db047793baa0e95f4
shinchaerin79 <shinchaerin79@gmail.com>
2025-03-27 15:45:19 +0900

    :recycle: Refactor: conflict test - local

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World! I don't think so");
+
+        System.out.println("Hello World! I don't think so.");
     }
 }
